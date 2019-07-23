64d4579bdeb154d4fca90b5f9c5b78bf8a475657
Alex Tilatti <alex.tilatti@gmail.com>
2019-07-22 21:03:38 -0400

    added custom text area for search, created new post route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,77 @@
-    import React, { Component } from 'react';
-    import Pusher from 'pusher-js';
-    import pushid from 'pushid';
-    import './App.css';
-    require('dotenv').config({ path: 'variables.env' });
+import React, { Component } from "react";
+import Pusher from "pusher-js";
+import pushid from "pushid";
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { newsItems: [], value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    alert("Subject Submitted:" + this.state.value);
+    var topic = this.state.value;
+    event.preventDefault();
 
-    class App extends Component {
-      state = {
-        newsItems: [],
+    fetch("http://localhost:5000/api/search", {
+      method: "POST",
+      body: JSON.stringify({ firstParam: topic }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
       }
+    })
+      .then(res => res.json())
+      .then(response => console.log("SUCCESS"));
+  }
 
-      componentDidMount() {
-        fetch('http://localhost:5000/live')
-          .then(response => response.json())
-          .then(articles => {
-            this.setState({
-              newsItems: [...this.state.newsItems, ...articles],
-            });
-          }).catch(error => console.log(error));
-
-        const pusher = new Pusher("7ebfc83d432373bb6d8c", { 
-          cluster: "us2",
-          encrypted: true,
+  componentDidMount() {
+    fetch("http://localhost:5000/live")
+      .then(response => response.json())
+      .then(articles => {
+        this.setState({
+          newsItems: [...this.state.newsItems, ...articles]
         });
+      })
+      .catch(error => console.log(error));
+    const pusher = new Pusher("7ebfc83d432373bb6d8c", {
+      cluster: "us2",
+      encrypted: true
+    });
+    const channel = pusher.subscribe("news-channel");
+    channel.bind("update-news", data => {
+      this.setState({
+        newsItems: [...data.articles, ...this.state.newsItems]
+      });
+    });
+  }
+  render() {
+    const NewsItem = (article, id) => (
+      <li key={id}>
+        <a href={`${article.url}`}>{article.description}</a>
+      </li>
+    );
+    const newsItems = this.state.newsItems.map(e => NewsItem(e, pushid()));
+    return (
+      <div className="App">
+        <h1 className="App-title">News Feed</h1>
+        <form className="news-search" onSubmit={this.handleSubmit}>
+          <input
+            className="submit-button"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+          <input className="submit-button" type="submit" value="Submit" />
+        </form>
+        <ul className="news-items">{newsItems}</ul>
+      </div>
+    );
+  }
+}
+export default App;
 
-        const channel = pusher.subscribe('news-channel');
-        channel.bind('update-news', data => {
-          this.setState({
-            newsItems: [...data.articles, ...this.state.newsItems],
-          });
-        });
-      }
-
-      render() {
-        const NewsItem = (article, id) => (
-          <li key={id}><a href={`${article.url}`}>{article.title}</a></li>
-        );
-
-        const newsItems = this.state.newsItems.map(e => NewsItem(e, pushid()));
-
-        return (
-          <div className="App">
-            <h1 className="App-title">Live Bitcoin Feed</h1>
-
-            <ul className="news-items">{newsItems}</ul>
-          </div>
-        );
-      }
-    }
-
-    export default App;
--- server.js
+++ server.js
@@ -1,60 +1,58 @@
 
+const express = require("express");
+const cors = require("cors");
+const Pusher = require("pusher");
+const NewsAPI = require("newsapi");
+const env = require("./my-app/variables.env");
+const app = express();
+const pusher = new Pusher({
+  appId: env.PUSHER_APP_ID,
+  key: env.PUSHER_APP_KEY,
+  secret: env.PUSHER_APP_SECRET,
+  cluster: env.PUSHER_APP_CLUSTER,
+  encrypted: true
+});
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
 
-    const express = require('express');
-    const cors = require('cors');
-    const Pusher = require('pusher');
-    const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(env.NEWS_API_KEY);
+const fetchNews = (searchTerm, pageNum) =>
+  newsapi.v2.everything({
+    q: searchTerm,
+    language: "en",
+    page: pageNum,
+    pageSize: 5
+  });
+app.use(cors());
+function updateFeed(topic) {
+  let counter = 2;
+  setInterval(() => {
+    fetchNews(topic, counter)
+      .then(response => {
+        pusher.trigger("news-channel", "update-news", {
+          articles: response.articles
+        });
+        counter += 1;
+      })
+      .catch(error => console.log(error));
+  }, 60000);
+}
+app.get("/live", (req, res) => {
+  const topic = "technology";
+  fetchNews(topic, 1)
+    .then(response => {
+      res.json(response.articles);
+      // updateFeed(topic);
+    })
+    .catch(error => console.log(error));
+});
 
-    const env = require("./my-app/variables.env")
+app.post("/api/search", (req, res) => {
+  let topic = req.body.firstParam
+  console.log("hellloooo", topic)
+})
 
-    const app = express();
-    console.log(env.PUSHER_APP_ID)
-    const pusher = new Pusher({
-      appId: env.PUSHER_APP_ID,
-      key: env.PUSHER_APP_KEY,
-      secret: env.PUSHER_APP_SECRET,
-      cluster: env.PUSHER_APP_CLUSTER,
-      encrypted: true,
-    });
-    console.log(pusher.appID)
-
-    const newsapi = new NewsAPI(env.NEWS_API_KEY);
-
-    const fetchNews = (searchTerm, pageNum) =>
-      newsapi.v2.everything({
-        q: searchTerm,
-        language: 'en',
-        page: pageNum,
-        pageSize: 5,
-      });
-
-    app.use(cors());
-
-    function updateFeed(topic) {
-      let counter = 2;
-      setInterval(() => {
-        fetchNews(topic, counter)
-          .then(response => {
-            pusher.trigger('news-channel', 'update-news', {
-              articles: response.articles,
-            });
-            counter += 1;
-          })
-          .catch(error => console.log(error));
-      }, 5000);
-    }
-
-    app.get('/live', (req, res) => {
-      const topic = 'alibaba';
-      fetchNews(topic, 1)
-        .then(response => {
-          res.json(response.articles);
-          updateFeed(topic);
-        })
-        .catch(error => console.log(error));
-    });
-
-    app.set('port', env.PORT || 5000);
-    const server = app.listen(app.get('port'), () => {
-      console.log(`Express running → PORT ${server.address().port}`);
-    });
+app.set("port", env.PORT || 5000);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
